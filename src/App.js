605ecd8d22f6963843c4f7d19eb73d729c699e23
usaid <mdusaid922@gmail.com>
2022-06-08 17:51:49 +0500

    commit it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AboutUs from './Components/AboutUs';
+import Alert from './Components/Alert';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm'
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+ 
+  Routes
+} from "react-router-dom";
 
-function App() {
+
+  function App() {
+    
+    const[alert ,setalert]=useState(null);
+    const showalt =(Message, type)=>{
+      setalert({
+        msg: Message,
+        type: type
+      })
+      setTimeout(() => {
+        showalt(null)
+      }, 3000);
+    }
+    const[mode ,setMode]=useState('light');
+    const togglemode =()=>{
+      if(mode==='light'){
+        setMode('dark');
+        document.body.style.backgroundColor ='#001a4d';
+        showalt("dark mode has been enabled","success");
+      }
+      else{
+        setMode('light');
+        document.body.style.backgroundColor ='white';
+        showalt("light mode has been enabled","success");
+      }
+    }
+    // const comode =()=>{
+    //   if(mode==='light'){
+    //     setMode('red');
+    //     document.body.style.backgroundColor ='red';
+
+    //     showalt("dark mode has been enabled","success");
+
+    //   }
+    //   else{
+    //     setMode('light');
+    //     document.body.style.backgroundColor ='white';
+
+    //     showalt("light mode has been enabled","success");
+    //   }
+    // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<>  
+<BrowserRouter>
+    <Navbar title="TextUtils" mode={mode} togglemode={togglemode} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Routes>
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          <Route exact path="/AboutUs" element ={<AboutUs/>}/>
+          <Route exact path="/" element={<TextForm showalt={showalt} heading="Enter the text to analyze below" mode={mode}/>}/>        
+          </Routes>
     </div>
+    </BrowserRouter>
+</>
   );
 }
 
